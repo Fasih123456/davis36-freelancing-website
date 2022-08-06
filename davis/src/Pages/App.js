e98a8520e199e6c025503f35059da92e6e0a1e1b
Fasih <fasihislam1999@gmail.com>
2022-08-06 15:17:24 -0300

    started page 3

--- davis/src/Pages/App.js
+++ davis/src/Pages/App.js
@@ -4,6 +4,7 @@ import screen1 from './screen1';
 import Record from './newrecord';
 import Screen2 from './screen2';
 import NodeAdd from './nodeadd';
+import Screen3 from './screen3';
 
 const App = () => {
   return(
@@ -13,6 +14,7 @@ const App = () => {
               <Route path="/products/" exact  component={Screen2} />
               <Route path="/product-add/" exact component={Record} />
               <Route path="/node-add/" exact component={NodeAdd} />
+              <Route path="/screen3/" exact component={Screen3} />
             </React.Fragment>
           </BrowserRouter>
       </div>
