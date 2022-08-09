ad42f22a07690cbc059a477cb6f23815b43e1043
Fasih <fasihislam1999@gmail.com>
2022-08-09 17:44:11 -0300

    finished screen 3

--- davis/src/Pages/App.js
+++ davis/src/Pages/App.js
@@ -5,6 +5,7 @@ import Record from './newrecord';
 import Screen2 from './screen2';
 import NodeAdd from './nodeadd';
 import Screen3 from './screen3';
+import Screen4 from './screen4';
 
 const App = () => {
   return(
@@ -15,6 +16,7 @@ const App = () => {
               <Route path="/product-add/" exact component={Record} />
               <Route path="/node-add/" exact component={NodeAdd} />
               <Route path="/screen3/" exact component={Screen3} />
+              <Route path="/screen4/" excat component={Screen4} />
             </React.Fragment>
           </BrowserRouter>
       </div>
---
+++ davis/src/Pages/screen3.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import { Link } from "react-router-dom";
+import Slider from "@mui/material/Slider";
+
+var data = [];
+
+function Screen3() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://62ea7b1c3a5f1572e87ca9e9.mockapi.io/product").then((response) => {
+      setProduct(response.data);
+      console.log("here");
+    });
+  }, ["https://62ea7b1c3a5f1572e87ca9e9.mockapi.io/product"]);
+
+  const postData = (e) => {
+    e.preventDefault();
+    console.log(e);
+    axios.post('https://62ea7b1c3a5f1572e87ca9e9.mockapi.io/product', {
+        name: name,
+        value: value
+    })
+}
+
+  if (product) {
+    return <React.Fragment>
+      {getValue(product)}
+      <Link
+        to={{
+          pathname: "/screen4",
+          state: data
+        }}
+        >
+        <button>Run Values</button>
+        </Link>
+      </React.Fragment>;
+  } else {
+    return (
+      <div>
+        <h1>no products</h1>
+      </div>
+    );
+  }
+}
+
+function getValue(products) {
+  console.log(products[0]);
+  var hashmap = new Map();
+
+  const newProducts = products[0].edges;
+  const nodes = products[0].nodes;
+  //console.log(nodes);
+
+  const arr1 = nodes.map((product) => {
+    hashmap.set(product.id, product.name);
+  });
+  data = hashmap;
+
+  const arr = newProducts.map((product) => {
+    return (
+      <React.Fragment>
+        <div>
+          <h3>
+            Node {hashmap.get(product.source)} To {hashmap.get(product.target)}
+          </h3>
+
+          <Slider
+            size="small"
+            defaultValue={product.weight}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            min={0}
+            max={1}
+            step={0.01}
+          />
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return arr;
+}
+
+export default Screen3;
